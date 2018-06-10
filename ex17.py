c0b06879a18ca2e04d950ae586d786e3af49d0fd
sumyatphyo <msu962978@gmail.com>
2018-06-10 07:50:08 -0700

    exercises

---
+++ ex17.py
@@ -0,0 +1,12 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename} :")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
