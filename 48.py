8c6908db7564fd42f4a4b77e87ec78f3afd6e596
sumyatphyo <msu962978@gmail.com>
2018-06-19 12:02:58 -0700

    exercises

---
+++ 48.py
@@ -0,0 +1,10 @@
+def match(word_list, expecting):
+    if word_list:
+        word = word_list.pop(0)
+
+        if word[0] == expecting:
+            return word
+        else:
+            return None
+    else:
+        return None
