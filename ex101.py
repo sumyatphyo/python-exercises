02cd79ea8477f7592224c050dfefd72219d0320a
sumyatphyo <msu962978@gmail.com>
2018-06-26 02:25:25 -0700

    exercise101

---
+++ ex101.py
@@ -0,0 +1,14 @@
+tabby_dog = "\tI'm tabbed in"
+persian_dog = "I'm split\non a line."
+backslash_dog = "I'm \\ a \\ dog."
+
+fat_dog = """
+I'll do a list:
+\t* dog food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+print(tabby_dog)
+print(persian_dog)
+print(backslash_dog)
+print(fat_dog)
